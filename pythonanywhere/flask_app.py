723fcb5618757963a358db25d460e49c1539458c
Sid Sachs <87913200+kasilakeconsulting@users.noreply.github.com>
2021-12-03 11:57:41 -0500

    Add files via upload

--- pythonanywhere/flask_app.py
+++ pythonanywhere/flask_app.py
@@ -21,7 +21,7 @@ def forecast_lat_lon(lat="", lon=""):
 
 @app.after_request
 def after_request(response):
-    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+    response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET,POST'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     return response
